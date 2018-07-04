083c8d38677b77d90c1d1af08b504a746d8bd3d3
Yota Toyama <raviqqe@gmail.com>
2018-07-05 06:16:07 +0900

    Add bin component

---
+++ src/components/Bin.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Trash = require("react-icons/lib/fa/trash");
+
+export default class extends React.Component {
+    public render() {
+        return <Trash />;
+    }
+}
