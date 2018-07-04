618a88c537ffb626471871f6159b2988cb3e724f
Yota Toyama <raviqqe@gmail.com>
2018-07-05 03:56:20 +0900

    Add app component

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class extends React.Component {
+    public render() {
+        return (
+            <div>
+                Trashy Instructor
+            </div>
+        );
+    }
+}
