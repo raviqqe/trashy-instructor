3441a980a78a9b65ebc67a21629e0cffd259258f
Yota Toyama <raviqqe@gmail.com>
2018-07-06 09:41:32 +0900

    Animate Rubbish

--- src/components/Rubbish.tsx
+++ src/components/Rubbish.tsx
@@ -1,21 +1,58 @@
 import * as React from "react";
-import styled from "styled-components";
+import { connect } from "react-redux";
+import styled, { css } from "styled-components";
 
-import { rubbish, RubbishId } from "../domain";
+import { IMovement, rubbish, RubbishId } from "../domain";
+import { elementToPosition, positionsToMovement } from "../infra";
+import * as bins from "../state/bins";
+import * as keyboard from "../state/keyboard";
 
-const Rubbish = styled.div`
+const duration = 1000;
+
+const Rubbish = styled.div<{ movement: IMovement, moving: boolean }>`
     color: red;
     font-size: 10em;
+    transition: transform ${duration}ms;
+    ${({ movement: { x, y }, moving }) => moving ? `transform: translate(${x}px, ${y}px);` : ""}
 `;
 
-interface IProps {
+interface IProps extends Partial<keyboard.IState> {
+    bins?: bins.IState;
     id: RubbishId;
 }
 
-export default class extends React.Component<IProps> {
+interface IState {
+    movement: IMovement;
+    moving: boolean;
+}
+
+@connect(({ bins, keyboard }) => ({ bins, ...keyboard }))
+export default class extends React.Component<IProps, IState> {
+    public ref: any = React.createRef();
+    public state: IState = { movement: { x: 0, y: 0 }, moving: false };
+
     public render() {
+        const { movement, moving } = this.state;
         const Image = rubbish[this.props.id].image;
 
-        return <Rubbish><Image /></Rubbish>;
+        return (
+            <Rubbish innerRef={this.ref} movement={movement} moving={moving}>
+                <Image />
+            </Rubbish>
+        );
+    }
+
+    public componentDidUpdate(props: IProps) {
+        const { bins, currentKey, id } = this.props;
+        const { binId, key } = rubbish[id];
+
+        if (props.currentKey !== currentKey && key === currentKey) {
+            this.setState({
+                movement: positionsToMovement(elementToPosition(this.ref.current), bins[binId]),
+                moving: true,
+            });
+
+            setTimeout(() => this.setState({ moving: false }), duration);
+        }
     }
 }
--- src/components/__tests__/Rubbish.tsx
+++ src/components/__tests__/Rubbish.tsx
@@ -1,9 +1,11 @@
 import { shallow } from "enzyme";
 import * as React from "react";
+import { Provider } from "react-redux";
 
 import { RubbishId } from "../../domain";
+import createStore from "../../state";
 import Rubbish from "../Rubbish";
 
 test("Render", () => {
-    shallow(<Rubbish id={RubbishId.Apple} />);
+    shallow(<Provider store={createStore()}><Rubbish id={RubbishId.Apple} /></Provider>);
 });
