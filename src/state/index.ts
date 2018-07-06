91509694989b7a71f58d51e1b17df4d70243aa13
Yota Toyama <raviqqe@gmail.com>
2018-07-06 11:00:18 +0900

    Install redux-thunk

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,4 +1,5 @@
-import { combineReducers, createStore, Store } from "redux";
+import { applyMiddleware, combineReducers, createStore, Store } from "redux";
+import thunk from "redux-thunk";
 
 import * as bins from "./bins";
 import * as keyboard from "./keyboard";
@@ -7,7 +8,7 @@ export default function(): Store {
     const store = createStore(combineReducers({
         bins: bins.reducer,
         keyboard: keyboard.reducer,
-    }));
+    }), applyMiddleware(thunk));
 
     keyboard.initializeStore(store);
 
